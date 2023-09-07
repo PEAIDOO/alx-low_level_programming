0ae8a8c2645d162016459bc7c16d5840d925ea54
PEAIDOO <alsweleinstein@gmail.com>
2023-09-07 08:13:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - we program here
+ *
+ * Return: we return zero 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
