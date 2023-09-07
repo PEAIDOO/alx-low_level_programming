016a2793704fde48ca4004cb39990c450697e47f
PEAIDOO <alsweleinstein@gmail.com>
2023-09-07 07:00:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - here is the main program
+ * Return: Results in void
+ */
+
+int main(void)
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
