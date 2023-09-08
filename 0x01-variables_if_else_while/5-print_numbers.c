535bb79590e7892ef0ab77b0dd76900efef0124e
PEAIDOO <alsweleinstein@gmail.com>
2023-09-08 16:09:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main lives here
+ *
+ * Return: zero will be returned
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("a\n");
+	return (0);
+}
