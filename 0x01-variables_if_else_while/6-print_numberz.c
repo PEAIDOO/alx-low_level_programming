1416b3f0fe0ed117a4e002f2b2d924a05df7464b
PEAIDOO <alsweleinstein@gmail.com>
2023-09-08 16:25:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main lives here
+ *
+ * Return: zero lives here
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
