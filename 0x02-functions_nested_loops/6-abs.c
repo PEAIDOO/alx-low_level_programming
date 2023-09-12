bb71accc57d8caef2718001a738e0f173bf17b28
PEAIDOO <alsweleinstein@gmail.com>
2023-09-12 10:42:07 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - funtion
+ * @a: variable name
+ * Return: return something
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+	{
+		return (a);
+	}
+
+	return (0);
+}
